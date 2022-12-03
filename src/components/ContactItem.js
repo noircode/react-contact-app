0c989b81cf42ac8ff875efe4d1fdada2a9491cb9
Kang Noir <93761595+noircode@users.noreply.github.com>
2022-12-03 02:50:41 +0000

    added icons with react-icons

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ContactItemBody from './ContactItemBody';
 import ContactItemImage from './ContactItemImage';
 import DeleteButton from './DeleteButton';
-import PropTypes from 'prop-types'
 
-function ContactItem({ imageUrl, name, tag, id, onDelete }) {
- return (
-   <div className="contact-item">
-     <ContactItemImage imageUrl={imageUrl} />
-     <ContactItemBody name={name} tag={tag} />
-     <DeleteButton id={id} onDelete={onDelete} />
-   </div>
- );
+function ContactItem({
+  imageUrl, name, tag, id, onDelete,
+}) {
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div className="contact-item">
+      <ContactItemImage imageUrl={imageUrl} />
+      <ContactItemBody name={name} tag={tag} />
+      <DeleteButton id={id} onDelete={onDelete} />
+    </div>
+  );
 }
 
 ContactItem.propTypes = {
@@ -19,7 +22,7 @@ ContactItem.propTypes = {
   name: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
-  onDelete: PropTypes.func.isRequired
-}
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default ContactItem;
--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FiDelete } from 'react-icons/fi';
 
 function DeleteButton({ id, onDelete }) {
-  return <button className='contact-item__delete' onClick={() => onDelete(id)}>X</button>
+  // eslint-disable-next-line max-len
+  // eslint-disable-next-line jsx-a11y/control-has-associated-label, react/button-has-type, react/jsx-filename-extension
+  return <button className="contact-item__delete" onClick={() => onDelete(id)}><FiDelete /></button>;
 }
 
 DeleteButton.propTypes = {
   id: PropTypes.number.isRequired,
-  onDelete: PropTypes.func.isRequired
-}
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default DeleteButton;
